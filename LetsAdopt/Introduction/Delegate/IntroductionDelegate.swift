6f4c3235c4e7fb817c6e39c2fe835abcaa27e3c9
Joris Marhuenda <marhuendajoris@gmail.com>
2022-02-07 13:04:06 +0100

    Add the introduction delegate for skip the intro

---
+++ LetsAdopt/Introduction/Delegate/IntroductionDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  IntroductionDelegate.swift
+//  LetsAdopt
+//
+//  Created by marhuenda joris on 07/02/2022.
+//
+
+import Foundation
+
+protocol IntroductionDelegate: AnyObject {
+    func skipIntroduce()
+    func nextIntroduce()
+}
