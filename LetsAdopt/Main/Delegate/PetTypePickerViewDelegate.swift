de3904dd5c36eb7a79d476a9189fd05e9658aefe
Joris Marhuenda <marhuendajoris@gmail.com>
2022-02-15 13:18:14 +0100

    Add the pet picker choice and the sort picker choice

---
+++ LetsAdopt/Main/Delegate/PetTypePickerViewDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  PetTypePickerViewDelegate.swift
+//  LetsAdopt
+//
+//  Created by marhuenda joris on 15/02/2022.
+//
+
+import UIKit
+
+class PetTypePickerViewDelegate: NSObject, UIPickerViewDataSource, UIPickerViewDelegate {
+    let types = ["Dog", "Cat", "Other"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return types.count
+    }
+    
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return types[row]
+    }
+}
