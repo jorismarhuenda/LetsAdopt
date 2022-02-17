7a001589828c4f4ba254fe8d1ef299800a30e67f
Joris Marhuenda <marhuendajoris@gmail.com>
2022-02-17 13:02:07 +0100

    Continue the database manager

--- LetsAdopt/Main/Resources/ChatDataBaseManager.swift
+++ LetsAdopt/Main/Resources/ChatDataBaseManager.swift
@@ -12,6 +12,103 @@ import CoreLocation
 
 final class ChatDatabaseManager {
     
+    public static let shared = ChatDatabaseManager()
+    
+    let database = Database.database().reference()
+
+    static func safeEmail(emailAddress: String) -> String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
+}
+
+extension ChatDatabaseManager {
+
+    public func getDataFor(path: String, completion: @escaping (Result<Any, Error>) -> Void) {
+        database.child("\(path)").observeSingleEvent(of: .value) { snapshot in
+            guard let value = snapshot.value else {
+                completion(.failure(DatabaseError.failedToFetch))
+                return
+            }
+            completion(.success(value))
+        }
+    }
+
+}
+
+
+extension ChatDatabaseManager {
+
+    public func userExists(with email: String,
+                           completion: @escaping ((Bool) -> Void)) {
+
+        let safeEmail = ChatDatabaseManager.safeEmail(emailAddress: email)
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
+            guard snapshot.value as? [String: Any] != nil else {
+                completion(false)
+                return
+            }
+
+            completion(true)
+        })
+
+    }
+
+    public func insertUser(with user: ChatAppUser, completion: @escaping (Bool) -> Void) {
+        database.child(user.safeEmail).setValue([
+            "fullName": user.fullName,
+        ], withCompletionBlock: { [weak self] error, _ in
+
+            guard let strongSelf = self else {
+                return
+            }
+
+            guard error == nil else {
+                print("failed ot write to database")
+                completion(false)
+                return
+            }
+
+            strongSelf.database.child("users").observeSingleEvent(of: .value, with: { snapshot in
+                if var usersCollection = snapshot.value as? [[String: String]] {
+                    // append to user dictionary
+                    let newElement = [
+                        "name": user.fullName,
+                        "email": user.safeEmail
+                    ]
+                    usersCollection.append(newElement)
+
+                    strongSelf.database.child("users").setValue(usersCollection, withCompletionBlock: { error, _ in
+                        guard error == nil else {
+                            completion(false)
+                            return
+                        }
+
+                        completion(true)
+                    })
+                }
+                else {
+                    // create that array
+                    let newCollection: [[String: String]] = [
+                        [
+                            "name": user.fullName,
+                            "email": user.safeEmail
+                        ]
+                    ]
+
+                    strongSelf.database.child("users").setValue(newCollection, withCompletionBlock: { error, _ in
+                        guard error == nil else {
+                            completion(false)
+                            return
+                        }
+
+                        completion(true)
+                    })
+                }
+            })
+        })
+    }
 }
 
 struct ChatAppUser {
@@ -27,6 +124,17 @@ struct ChatAppUser {
     var profilePictureFileName: String {
         //afraz9-gmail-com_profile_picture.png
         return "\(safeEmail)_profile_picture.png"
+    }
+    
+    public enum DatabaseError: Error {
+        case failedToFetch
+
+        public var localizedDescription: String {
+            switch self {
+            case .failedToFetch:
+                return "This means blah failed"
+            }
+        }
     }
 }
 
